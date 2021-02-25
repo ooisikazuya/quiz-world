bfe581c35d19519e47cc2b8ad1326d3684b76f14
ooisikazuya <i1no1su6ke@gmail.com>
2021-02-25 17:48:05 +0900

    devise導入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,27 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user, only: [:show, :edit, :update]
   
   def show
+    
   end
 
   def edit
   end
 
   def update
+    @user.update(user_params)
+    flash[:notice] = "会員情報を更新しました！"
+    redirect_to user_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :image)
+  end
+
+  def set_user
+    @user = current_user
   end
 end
