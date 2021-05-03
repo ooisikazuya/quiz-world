bf779f4ed48d6deefaa482550984c49fea8656cb
EC2 Default User <ec2-user@ip-172-30-2-62.ap-northeast-1.compute.internal>
2021-05-03 12:52:10 +0000

    test: Quiz.search

--- spec/models/quiz_spec.rb
+++ spec/models/quiz_spec.rb
@@ -47,4 +47,30 @@ RSpec.describe Quiz, type: :model do
       end
     end
   end
+  
+  describe 'search' do
+    let!(:quiz_1) { create(:quiz, genre: 'アニメ1') }
+    let!(:quiz_2) { create(:quiz, genre: 'アニメ2') }
+    let!(:quiz_3) { create(:quiz) }
+    let!(:quiz_4) { create(:quiz, status: 'before_release') }
+    let!(:quiz_5) { create(:quiz, status: 'deleted') }
+    
+    subject { Quiz.search(search) }
+    
+    context 'searchがnilの場合' do
+      let(:search) { nil }
+      
+      it 'releasedのQuizをすべて返す' do
+        expect(subject.map(&:id)).to match [quiz_1.id, quiz_2.id, quiz_3.id]
+      end
+    end
+    
+    context 'searchがnil出ない場合' do
+      let(:search) { 'アニメ' }
+      
+      it 'genreにsearchの文字を含むreleasedのQuizをすべて返す' do
+        expect(subject.map(&:id)).to match [quiz_1.id, quiz_2.id]
+      end
+    end
+  end
 end
