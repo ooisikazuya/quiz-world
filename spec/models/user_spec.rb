f6d1b17e925505ff65e1171c38448beca7ac18db
ooisikazuya <i1no1su6ke@gmail.com>
2021-05-03 10:54:27 +0900

    userのmodelテスト追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,4 +24,27 @@ RSpec.describe User, type: :model do
     expect(user.errors[:nickname]).to include("が入力されていません。")
   end
 
+  it "メールアドレスがない場合、無効である" do
+    user = User.new(
+      nickname: "testman",
+      email: nil,
+      password: "password"
+      )
+
+    user.valid?
+
+    expect(user.errors[:email]).to include("が入力されていません。")
+  end
+
+  it "パスワードがない場合、無効である" do
+    user = User.new(
+      nickname: "testman",
+      email: "testman@example.com",
+      password: nil
+      )
+
+    user.valid?
+
+    expect(user.errors[:password]).to include("が入力されていません。")
+  end
 end
