a28e7760a60dcb8cb4250f899fb5703791b0ab2a
EC2 Default User <ec2-user@ip-172-30-2-62.ap-northeast-1.compute.internal>
2021-05-03 12:33:00 +0000

    refactor quiz_spec

--- spec/models/quiz_spec.rb
+++ spec/models/quiz_spec.rb
@@ -1,42 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Quiz, type: :model do
-  before do
-    @user = User.create(
-      id: 1,
-      nickname: "testman",
-      email: "testman@example.com",
-      password: "password"
-    )
-  end
-
-  it "クイズ名、ジャンルがある場合、有効である" do
-    quiz = Quiz.new(
-      user_id: @user.id,
-      title: "銀魂クイズ",
-      genre: "銀魂"
+  describe 'validation' do
+    before do
+      @user = User.create(
+        id: 1,
+        nickname: "testman",
+        email: "testman@example.com",
+        password: "password"
       )
-
-    expect(quiz).to be_valid
-  end
-
-  it "クイズ名がない場合、無効である" do
-    quiz = Quiz.new(
-      user_id: @user.id,
-      title: nil,
-      genre: "銀魂"
-      )
-
-    expect(quiz).not_to be_valid
-  end
-
-  it "ジャンルがない場合、無効である" do
-    quiz = Quiz.new(
-      user_id: @user.id,
-      title: "銀魂クイズ",
-      genre: nil
-      )
-
-    expect(quiz).not_to be_valid
+    end
+    
+    context 'クイズ名、ジャンルがある場合' do
+      it "有効である" do
+        quiz = Quiz.new(
+          user_id: @user.id,
+          title: "銀魂クイズ",
+          genre: "銀魂"
+          )
+    
+        expect(quiz).to be_valid
+      end
+    end
+  
+    context 'クイズ名がない場合' do
+      it "無効である" do
+        quiz = Quiz.new(
+          user_id: @user.id,
+          title: nil,
+          genre: "銀魂"
+          )
+    
+        expect(quiz).not_to be_valid
+      end
+    end
+  
+    context 'ジャンルがない場合' do
+      it "無効である" do
+        quiz = Quiz.new(
+          user_id: @user.id,
+          title: "銀魂クイズ",
+          genre: nil
+          )
+    
+        expect(quiz).not_to be_valid
+      end
+    end
   end
 end
